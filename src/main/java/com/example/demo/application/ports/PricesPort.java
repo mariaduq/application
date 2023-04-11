c016bc1d8d05e63ae7124018bc3a98715f03a768
María Duque <mariaduq01@gmail.com>
2023-04-11 20:07:22 +0200

    fix: some changes to try to solve some errors

--- src/main/java/com/example/demo/application/ports/PricesPort.java
+++ src/main/java/com/example/demo/application/ports/PricesPort.java
@@ -2,8 +2,11 @@ package com.example.demo.application.ports;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import com.example.demo.model.Price;
 
+@Component
 public interface PricesPort {
 	
 	public List<Price> getPrices();
