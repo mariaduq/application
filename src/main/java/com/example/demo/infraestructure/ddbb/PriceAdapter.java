dffb9f735b7f1ec1f420ab048b186848839a4711
María Duque <mariaduq01@gmail.com>
2023-04-13 09:47:06 +0200

    fix: annotation @Service in PriceAdapter

--- src/main/java/com/example/demo/infraestructure/ddbb/PriceAdapter.java
+++ src/main/java/com/example/demo/infraestructure/ddbb/PriceAdapter.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.application.ports.PricesPort;
 import com.example.demo.model.Price;
 import com.example.demo.model.PriceRepository;
 
+@Service
 public class PriceAdapter implements PricesPort{
 	
 	private PriceRepository priceRepository;
