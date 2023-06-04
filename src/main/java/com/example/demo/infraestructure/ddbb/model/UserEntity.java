11e582f74c514cea76efef22990e5e60935dd50d
María Duque <maria.duque@kairosds.com>
2023-06-04 20:16:56 +0200

    feat: new class UserEntity

---
+++ src/main/java/com/example/demo/infraestructure/ddbb/model/UserEntity.java
@@ -0,0 +1,36 @@
+package com.example.demo.infraestructure.ddbb.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "USERS")
+@AllArgsConstructor
+@NoArgsConstructor
+public @Data class UserEntity {
+
+    @Id
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "SURNAME")
+    private String surname;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "ROL")
+    private int rol;
+
+}
