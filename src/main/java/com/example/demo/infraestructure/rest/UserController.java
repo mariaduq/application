041952426238ee5977a4f1802111b25cd564f46b
María Duque <mariaduq01@gmail.com>
2023-07-04 14:09:27 +0200

    feat: user controller

---
+++ src/main/java/com/example/demo/infraestructure/rest/UserController.java
@@ -0,0 +1,40 @@
+package com.example.demo.infraestructure.rest;
+
+import com.example.demo.application.SaveUserUseCase;
+import com.example.demo.infraestructure.ddbb.PriceRepositoryJpa;
+import com.example.demo.infraestructure.ddbb.UserRepositoryJpa;
+import com.example.demo.model.User;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/signup")
+public class UserController {
+
+    @Autowired
+    UserRepositoryJpa userRepositoryJpa;
+
+    @Autowired
+    private SaveUserUseCase saveUserUseCase;
+
+    @ModelAttribute("user")
+    public ResponseEntity<UserDTO> saveUser() {
+        return ResponseEntity.ok().body(new UserDTO());
+    }
+
+    @GetMapping
+    public String showRegistryForm() {
+        return "user-form";
+    }
+
+    @PostMapping
+    public String registerNewUserAccount(@ModelAttribute("user") UserDTO userDTO) {
+        ModelMapper mapper = new ModelMapper();
+        User user = mapper.map(userDTO, User.class);
+        saveUserUseCase.execute(user);
+
+        return "redirect:/user-form?success";
+    }
+}
