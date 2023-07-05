9de92fc5e22b06fc3d51048d9cc0e31a059966a6
María Duque <mariaduq01@gmail.com>
2023-07-05 11:41:12 +0200

    feat: add method to jpa user repository

--- src/main/java/com/example/demo/infraestructure/ddbb/UserRepositoryJpa.java
+++ src/main/java/com/example/demo/infraestructure/ddbb/UserRepositoryJpa.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.infraestructure.ddbb.model.UserEntity;
 
+import java.util.Optional;
+
 public interface UserRepositoryJpa extends JpaRepository<UserEntity, Long> {
-    
+    Optional<UserEntity> findByNicknameOrEmail(String nickname, String email);
 }
