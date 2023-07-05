5d176b18d1ac7b5ea7c78c8ff399ec734adc1abb
María Duque <mariaduq01@gmail.com>
2023-07-05 14:24:47 +0200

    feat: check nickname, email and password

--- src/main/java/com/example/demo/infraestructure/ddbb/UserAdapter.java
+++ src/main/java/com/example/demo/infraestructure/ddbb/UserAdapter.java
@@ -1,23 +1,43 @@
 package com.example.demo.infraestructure.ddbb;
 
+import com.example.demo.application.UserInput;
 import com.example.demo.application.ports.UsersPort;
 import com.example.demo.infraestructure.ddbb.mappers.UserMapper;
+import com.example.demo.infraestructure.ddbb.model.UserEntity;
 import com.example.demo.model.User;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@AllArgsConstructor
 public class UserAdapter implements UsersPort {
 
-    private UserRepositoryJpa userRepositoryJpa;
+    private final UserRepositoryJpa userRepositoryJpa;
 
-    private UserMapper userMapper;
-
-    public UserAdapter(UserRepositoryJpa userRepositoryJpa) {
-        this.userRepositoryJpa = userRepositoryJpa;
-    }
+    private final UserMapper userMapper;
 
     @Override
-    public User save(User userRegistry) {
-        return userMapper.toDomain(userRepositoryJpa.save(userMapper.toEntity(userRegistry)));
+    public User save(User newUser) throws Exception {
+        if(checkNicknameAndEmailAvailable(newUser) && checkPasswordMatch(newUser)) {
+            return userMapper.toDomain(userRepositoryJpa.save(userMapper.toEntity(newUser)));
+        }
+        return newUser;
+    }
+
+    private boolean checkNicknameAndEmailAvailable(User user) throws Exception {
+        Optional<UserEntity> userFound = userRepositoryJpa.findByNicknameOrEmail(user.getNickname(), user.getEmail());
+        if(userFound.isPresent()) {
+            throw new Exception("Username or email already exists");
+        }
+        return true;
+    }
+
+    private boolean checkPasswordMatch(User user) throws Exception {
+        if(!user.getPassword().equals(user.getConfirmPassword())) {
+            throw new Exception("Passwords don't match");
+        }
+        return true;
     }
 }
