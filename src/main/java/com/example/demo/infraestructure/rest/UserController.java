dbc309bb26eaff5fba47f333adc2f05448ab21c7
María Duque <mariaduq01@gmail.com>
2023-07-05 14:23:06 +0200

    feat: create a new user

--- src/main/java/com/example/demo/infraestructure/rest/UserController.java
+++ src/main/java/com/example/demo/infraestructure/rest/UserController.java
@@ -3,41 +3,49 @@ package com.example.demo.infraestructure.rest;
 import com.example.demo.application.SaveUserUseCase;
 import com.example.demo.infraestructure.ddbb.PriceRepositoryJpa;
 import com.example.demo.infraestructure.ddbb.UserRepositoryJpa;
+import com.example.demo.infraestructure.rest.mappers.UserMapper;
 import com.example.demo.model.User;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/signup")
+@RequiredArgsConstructor
 public class UserController {
-
-    @Autowired
-    UserRepositoryJpa userRepositoryJpa;
 
     @Autowired
     private SaveUserUseCase saveUserUseCase;
 
-    @ModelAttribute("user")
-    public ResponseEntity<UserDTO> saveUser() {
-        return ResponseEntity.ok().body(new UserDTO());
-    }
+    private final UserMapper userMapper;
 
     @GetMapping
-    public String getUserModel(Model model) {
+    public String getUserForm(Model model) {
         model.addAttribute("user", new UserDTO());
         return "user-form";
     }
 
     @PostMapping
-    public String registerNewUserAccount(@ModelAttribute("user") UserDTO userDTO) {
-        ModelMapper mapper = new ModelMapper();
-        User user = mapper.map(userDTO, User.class);
-        saveUserUseCase.execute(user);
+    public String createUser(@Valid @ModelAttribute("user") UserDTO userDTO, BindingResult result, ModelMap model) {
+        if(result.hasErrors()) {
+            model.addAttribute("user", userDTO);
+        }
+        else{
+            try{
+                saveUserUseCase.execute(userMapper.toUserInput(userDTO));
+            } catch (Exception e) {
+                model.addAttribute("formErrorMessage", e.getMessage());
+                model.addAttribute("user", userDTO);
+            }
 
-        return "redirect:/user-form?exito";
+        }
+        return "user-form";
     }
 }
