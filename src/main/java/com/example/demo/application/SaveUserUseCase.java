9513bbc79260cc8a4437d3ef728d7016fa9a3656
María Duque <mariaduq01@gmail.com>
2023-07-05 14:25:11 +0200

    feat: save user

--- src/main/java/com/example/demo/application/SaveUserUseCase.java
+++ src/main/java/com/example/demo/application/SaveUserUseCase.java
@@ -1,17 +1,19 @@
 package com.example.demo.application;
 
+import com.example.demo.application.mapper.UserMapper;
 import com.example.demo.application.ports.UsersPort;
 import com.example.demo.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 public class SaveUserUseCase {
 
-    private UsersPort usersPort;
+    private final UsersPort usersPort;
 
-    public SaveUserUseCase(UsersPort usersPort){
-        this.usersPort=usersPort;
-    }
+    private final UserMapper userMapper;
 
-    public User execute(User user) {
-        return usersPort.save(user);
+    public User execute(UserInput userInput) throws Exception {
+        return usersPort.save(userMapper.fromInput(userInput));
     }
 }
