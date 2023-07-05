4b5443c45e13761ec8a714b7578fe778e4510b02
María Duque <mariaduq01@gmail.com>
2023-07-05 14:23:58 +0200

    feat: throws exception

--- src/main/java/com/example/demo/application/ports/UsersPort.java
+++ src/main/java/com/example/demo/application/ports/UsersPort.java
@@ -1,8 +1,9 @@
 package com.example.demo.application.ports;
 
+import com.example.demo.application.UserInput;
 import com.example.demo.model.User;
 
 public interface UsersPort {
 
-    public User save(User userRegistry);
+    public User save(User newUser) throws Exception;
 }
