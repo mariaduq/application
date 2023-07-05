8f31a6649ca49c2c98563a2d51246820d947d1f8
María Duque <mariaduq01@gmail.com>
2023-07-05 14:23:30 +0200

    feat: UserInput class

---
+++ src/main/java/com/example/demo/application/UserInput.java
@@ -0,0 +1,23 @@
+package com.example.demo.application;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserInput {
+
+    private Long id;
+
+    private String nickname;
+
+    private String name;
+
+    private String surname;
+
+    private String email;
+
+    private String password;
+
+    private String confirmPassword;
+}
