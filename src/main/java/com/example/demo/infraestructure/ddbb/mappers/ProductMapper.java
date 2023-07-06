ad6d4e023551d17f386cf39d16a0ba513ce09ed4
María Duque <mariaduq01@gmail.com>
2023-07-06 18:05:20 +0200

    feat: product infrastructure mapper

---
+++ src/main/java/com/example/demo/infraestructure/ddbb/mappers/ProductMapper.java
@@ -0,0 +1,45 @@
+package com.example.demo.infraestructure.ddbb.mappers;
+
+import com.example.demo.infraestructure.ddbb.model.ProductEntity;
+import com.example.demo.model.Product;
+import com.example.demo.model.User;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+@NoArgsConstructor
+public class ProductMapper {
+
+    public Product toDomain(ProductEntity productEntity) {
+        if(Objects.isNull(productEntity)) {
+            return null;
+        }
+
+        return Product.builder()
+                .productId(productEntity.getProductId())
+                .productType(productEntity.getProductType())
+                .brand(productEntity.getBrand())
+                .brandId(productEntity.getBrandId())
+                .description(productEntity.getDescription())
+                .stock(productEntity.getStock())
+                .productPrices(productEntity.getProductPrices())
+                .build();
+    }
+
+    public ProductEntity toEntity(Product product) {
+        if(Objects.isNull(product)) {
+            return null;
+        }
+
+        return ProductEntity.builder()
+                .productType(product.getProductType())
+                .brand(product.getBrand())
+                .brandId(product.getBrandId())
+                .description(product.getDescription())
+                .stock(product.getStock())
+                .productPrices(product.getProductPrices())
+                .build();
+    }
+}
