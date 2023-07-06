5af9e004da0bae0663a1dd0445ac4484b10d8d36
María Duque <mariaduq01@gmail.com>
2023-07-06 13:40:30 +0200

    feat: implement login function

--- src/main/java/com/example/demo/infraestructure/ddbb/UserAdapter.java
+++ src/main/java/com/example/demo/infraestructure/ddbb/UserAdapter.java
@@ -5,6 +5,7 @@ import com.example.demo.application.ports.UsersPort;
 import com.example.demo.infraestructure.ddbb.mappers.UserMapper;
 import com.example.demo.infraestructure.ddbb.model.UserEntity;
 import com.example.demo.model.User;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -32,6 +33,17 @@ public class UserAdapter implements UsersPort {
         return newUser;
     }
 
+    @Override
+    public User login(User userLogin) throws Exception {
+        System.out.println(userLogin.getEmail() + "Es el email que hay que buscar");
+        Optional<UserEntity> userFound = userRepositoryJpa.findByEmail(userLogin.getEmail());
+        if(userFound.isPresent()){
+            if(checkPassword(userMapper.toDomain(userFound.get()), userLogin.getPassword())) {
+                return userMapper.toDomain(userFound.get());
+            } else throw new Exception("Incorrect password. Try again.");
+        } else throw new EntityNotFoundException("There is no user registered with this email. Try again.");
+    }
+
     private boolean checkNicknameAndEmailAvailable(User user) throws Exception {
         Optional<UserEntity> userFound = userRepositoryJpa.findByNicknameOrEmail(user.getNickname(), user.getEmail());
         if(userFound.isPresent()) {
@@ -45,5 +57,10 @@ public class UserAdapter implements UsersPort {
             throw new Exception("Passwords don't match");
         }
         return true;
+    }
+
+    private boolean checkPassword(User userFound, String loginPassword) {
+        if(userFound.getPassword().equals(loginPassword)) return true;
+        else return false;
     }
 }
