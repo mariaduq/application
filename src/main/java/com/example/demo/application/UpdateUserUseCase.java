f0563cf09abcd80b6e20f95c537c0050b7d75fae
María Duque <mariaduq01@gmail.com>
2023-07-07 13:01:31 +0200

    feat: update user use case

---
+++ src/main/java/com/example/demo/application/UpdateUserUseCase.java
@@ -0,0 +1,19 @@
+package com.example.demo.application;
+
+import com.example.demo.application.mapper.UserMapper;
+import com.example.demo.application.ports.UsersPort;
+import com.example.demo.model.User;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UpdateUserUseCase {
+
+    private final UsersPort usersPort;
+
+    private final UserMapper userMapper;
+
+    public User execute(UserInput userInput) throws Exception {
+        System.out.println(userInput);
+        return usersPort.update(userMapper.fromInput(userInput));
+    }
+}
