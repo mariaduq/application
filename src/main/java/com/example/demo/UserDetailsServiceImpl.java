1ddfa901a2229fa5d6715dfe05d2e01ad564f658
María Duque <mariaduq01@gmail.com>
2023-07-08 11:53:30 +0200

    feat: spring security

---
+++ src/main/java/com/example/demo/UserDetailsServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import com.example.demo.infraestructure.ddbb.UserRepositoryJpa;
+import com.example.demo.infraestructure.ddbb.model.UserEntity;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Transactional
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepositoryJpa userRepositoryJpa;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity user = userRepositoryJpa.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("No existe usuario"));
+
+        Set<GrantedAuthority> grantList = new HashSet<GrantedAuthority>();
+
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority("USER");
+        grantList.add(grantedAuthority);
+
+        UserDetails userDetails = (UserDetails) new User(username, user.getPassword(), grantList);
+
+        return userDetails;
+    }
+}
---
+++ src/main/java/com/example/demo/infraestructure/config/WebSecurityConfig.java
@@ -0,0 +1,63 @@
+package com.example.demo.infraestructure.config;
+
+import com.example.demo.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    String[] resources = new String[] {
+            "/include/**", "/css/**", "/icons/**", "/img/**", "/templates**"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest()
+                .permitAll()
+                .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .defaultSuccessUrl("/loggedUser")
+                    .failureUrl("/login?error=true")
+                    .usernameParameter("email")
+                    .passwordParameter("password")
+                    .and()
+                .logout()
+                .deleteCookies("JSESSIONID")
+                    .logoutUrl("/login?logout")
+                    .logoutSuccessUrl("/login?logout");
+
+        return http.build();
+    }
+
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public BCryptPasswordEncoder passwordEncoder() {
+        bCryptPasswordEncoder = new BCryptPasswordEncoder(4);
+        return bCryptPasswordEncoder;
+    }
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+}
