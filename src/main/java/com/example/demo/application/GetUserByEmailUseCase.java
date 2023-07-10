4a71dab4189ed7686087cbad9bd9a36c664073e7
María Duque <mariaduq01@gmail.com>
2023-07-10 11:45:02 +0200

    feat: get user by email use case

---
+++ src/main/java/com/example/demo/application/GetUserByEmailUseCase.java
@@ -0,0 +1,15 @@
+package com.example.demo.application;
+
+import com.example.demo.application.ports.UsersPort;
+import com.example.demo.model.User;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class GetUserByEmailUseCase {
+
+    private final UsersPort usersPort;
+
+    public User execute(String email) throws Exception {
+        return usersPort.getUserByEmail(email);
+    }
+}
