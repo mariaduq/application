80fb26ea1e46d1137dbade9caa4fbb6ecb871e47
María Duque <mariaduq01@gmail.com>
2023-07-11 17:26:33 +0200

    feat: get list of products

---
+++ src/main/java/com/example/demo/application/GetAllProductsUseCase.java
@@ -0,0 +1,18 @@
+package com.example.demo.application;
+
+import com.example.demo.application.ports.ProductsPort;
+import com.example.demo.application.ports.UsersPort;
+import com.example.demo.model.Product;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class GetAllProductsUseCase {
+
+    private final ProductsPort productsPort;
+
+    public List<Product> execute() {
+        return productsPort.getAllProducts();
+    }
+}
--- src/main/java/com/example/demo/application/ports/ProductsPort.java
+++ src/main/java/com/example/demo/application/ports/ProductsPort.java
@@ -1,4 +1,10 @@
 package com.example.demo.application.ports;
 
+import com.example.demo.model.Product;
+
+import java.util.List;
+
 public interface ProductsPort {
+
+    public List<Product> getAllProducts();
 }
