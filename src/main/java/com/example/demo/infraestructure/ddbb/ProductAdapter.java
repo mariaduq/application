3e001cd1ff6fe3e592ce3daab4a81e441732df79
María Duque <mariaduq01@gmail.com>
2023-07-11 17:25:56 +0200

    feat: product adapter

---
+++ src/main/java/com/example/demo/infraestructure/ddbb/ProductAdapter.java
@@ -0,0 +1,27 @@
+package com.example.demo.infraestructure.ddbb;
+
+import com.example.demo.application.ports.ProductsPort;
+import com.example.demo.infraestructure.ddbb.mappers.ProductMapper;
+import com.example.demo.model.Product;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class ProductAdapter implements ProductsPort {
+
+    private final ProductRepositoryJpa productRepositoryJpa;
+
+    private final ProductMapper productMapper;
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepositoryJpa.findAll()
+                .stream()
+                .map(productMapper::toDomain)
+                .collect(Collectors.toList());
+    }
+}
