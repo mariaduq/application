0021662da588371dfabc6e82a86c08a2db5d9790
María Duque <mariaduq01@gmail.com>
2023-07-11 17:27:31 +0200

    feat: product controller

---
+++ src/main/java/com/example/demo/infraestructure/rest/ProductController.java
@@ -0,0 +1,22 @@
+package com.example.demo.infraestructure.rest;
+
+import com.example.demo.application.GetAllProductsUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ProductController {
+
+    @Autowired
+    private final GetAllProductsUseCase getAllProductsUseCase;
+
+    @GetMapping("/productsList")
+    public String getProductsList(Model model) {
+        model.addAttribute("productsList", getAllProductsUseCase.execute());
+        return "products-list";
+    }
+}
