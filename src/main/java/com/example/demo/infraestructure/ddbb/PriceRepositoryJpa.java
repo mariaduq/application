6a0d88c489cf2c1c2a0c9d567ef652d54a6c96bb
María Duque <mariaduq01@gmail.com>
2023-07-11 18:11:35 +0200

    feat: find by productId

--- src/main/java/com/example/demo/infraestructure/ddbb/PriceRepositoryJpa.java
+++ src/main/java/com/example/demo/infraestructure/ddbb/PriceRepositoryJpa.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.infraestructure.ddbb.model.PriceEntity;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PriceRepositoryJpa extends JpaRepository<PriceEntity, Long> {
-	
+    Optional <List<PriceEntity>> findByProductId(Long productId);
 }
