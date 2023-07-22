dd24cb09baf22a698cb77309023417b4fd707f3d
María Duque <mariaduq01@gmail.com>
2023-07-22 15:11:18 +0200

    refactor: from output to dto

--- src/main/java/com/example/demo/infraestructure/rest/controller/ProductController.java
+++ src/main/java/com/example/demo/infraestructure/rest/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.example.demo.infraestructure.rest.controller;
 
+import com.example.demo.application.output.UserOutput;
 import com.example.demo.application.usecases.product.GetAllProductsUseCase;
 import com.example.demo.application.usecases.user.GetUserByEmailUseCase;
 import com.example.demo.domain.model.User;
+import com.example.demo.infraestructure.rest.mappers.ProductMapper;
+import com.example.demo.infraestructure.rest.mappers.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -11,6 +14,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,17 +28,24 @@ public class ProductController {
     @Autowired
     private GetUserByEmailUseCase getUserByEmailUseCase;
 
+    private final UserMapper userMapper;
+
+    private final ProductMapper productMapper;
+
     @GetMapping("/list")
     public String getProductsList(Authentication auth, Model model) throws Exception {
         if(auth != null) {
             UserDetails userDetails = (UserDetails) auth.getPrincipal();
             String email = userDetails.getUsername();
 
-            User loggedUser = getUserByEmailUseCase.execute(email);
-            model.addAttribute("user", loggedUser);
+            UserOutput loggedUser = getUserByEmailUseCase.execute(email);
+            model.addAttribute("user", userMapper.fromUserOutputToUserDTO(loggedUser));
 
             try{
-                model.addAttribute("productsList", getAllProductsUseCase.execute());
+                model.addAttribute("productsList", getAllProductsUseCase.execute()
+                        .stream()
+                        .map(productMapper::fromProductOutputToProductDTO)
+                        .collect(Collectors.toList()));
             } catch (Exception e) {
                 model.addAttribute("formErrorMessage", e.getMessage());
             }
