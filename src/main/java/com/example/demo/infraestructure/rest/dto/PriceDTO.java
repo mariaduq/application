f5e7217a460936c14cd257b2bfdab2da0cbf6fc8
María Duque <mariaduq01@gmail.com>
2023-07-22 15:10:46 +0200

    fix: add priority to PriceDTO

--- src/main/java/com/example/demo/infraestructure/rest/dto/PriceDTO.java
+++ src/main/java/com/example/demo/infraestructure/rest/dto/PriceDTO.java
@@ -1,7 +1,15 @@
 package com.example.demo.infraestructure.rest.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public @Data class PriceDTO {
 	
 	private long priceList;
@@ -13,7 +21,9 @@ public @Data class PriceDTO {
 	private long brandId;
 	
 	private long productId;
-		
+
+	private int priority;
+
 	private String price;
 
 	public void setPrice(float price) {
