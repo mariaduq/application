a807cdd96d0f4c568206ec8309a8a5d876d677f1
María Duque <mariaduq01@gmail.com>
2023-07-23 20:54:49 +0200

    feat: get products by brand use case

---
+++ src/main/java/com/example/demo/application/usecases/product/GetProductsByBrandUseCase.java
@@ -0,0 +1,28 @@
+package com.example.demo.application.usecases.product;
+
+import com.example.demo.application.mapper.ProductMapper;
+import com.example.demo.application.output.ProductOutput;
+import com.example.demo.domain.port.ProductsPort;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class GetProductsByBrandUseCase {
+
+    private final ProductsPort productsPort;
+
+    private final ProductMapper productMapper;
+
+    public List<ProductOutput> execute(String brandName) throws Exception {
+        List<ProductOutput> list = productsPort.getAllProducts()
+                .stream()
+                .filter(product -> product.getBrandName().equals(brandName))
+                .map(productMapper::toOutput)
+                .toList();
+
+        if (list.isEmpty()) throw new Exception("No tenemos ningún producto de esa marca.");
+        return list;
+    }
+}
