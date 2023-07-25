827eb47841a4a8f389458618d0cf992fe862fc9d
María Duque <mariaduq01@gmail.com>
2023-07-25 12:11:12 +0200

    refactor: delete unused import

--- src/test/java/com/example/demo/application/usecases/price/GetAllPricesUseCaseTest.java
+++ src/test/java/com/example/demo/application/usecases/price/GetAllPricesUseCaseTest.java
@@ -12,7 +12,6 @@ import java.sql.Timestamp;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 public class GetAllPricesUseCaseTest {
