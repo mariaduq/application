2009c9fd497a537c272c9c4d6a4a47a3c97f8b3f
María Duque <mariaduq01@gmail.com>
2023-07-25 12:00:23 +0200

    feat: add lombok annotations to price class

--- src/main/java/com/example/demo/domain/model/Price.java
+++ src/main/java/com/example/demo/domain/model/Price.java
@@ -2,10 +2,14 @@ package com.example.demo.domain.model;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public @Data class Price {
 	
 	private long priceList;
