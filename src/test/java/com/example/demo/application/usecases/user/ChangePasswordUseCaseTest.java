0ea34cf8b8b3aa93adc617b196b0e0f13f8bb29f
María Duque <mariaduq01@gmail.com>
2023-07-25 19:19:09 +0200

    test: testing ChangePasswordUseCase

---
+++ src/test/java/com/example/demo/application/usecases/user/ChangePasswordUseCaseTest.java
@@ -0,0 +1,60 @@
+package com.example.demo.application.usecases.user;
+
+import com.example.demo.infrastructure.persistence.adapter.UserAdapter;
+import com.example.demo.infrastructure.persistence.entities.UserEntity;
+import com.example.demo.infrastructure.persistence.jpa.UserRepositoryJpa;
+import com.example.demo.infrastructure.persistence.mappers.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class ChangePasswordUseCaseTest {
+
+    UserRepositoryJpa userRepositoryJpa = Mockito.mock(UserRepositoryJpa.class);
+
+    BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(4);
+
+    UserAdapter userAdapter = new UserAdapter(userRepositoryJpa, new UserMapper(), bCryptPasswordEncoder);
+
+    ChangePasswordUseCase changePasswordUseCase = new ChangePasswordUseCase(userAdapter);
+
+    @Test
+    void should_change_password_from_123_to_1234() throws Exception {
+        //GIVEN
+        String oldPassword = "123";
+        String encodedPassword = bCryptPasswordEncoder.encode(oldPassword);
+        String newPassword = "1234";
+        String email = "i92durom@uco.es";
+
+        when(userRepositoryJpa.findByEmail(any()))
+                .thenReturn(Optional.of(buildUser(encodedPassword)));
+        when(userRepositoryJpa.save(any()))
+                .thenReturn(buildUser(newPassword));
+
+        //WHEN
+        changePasswordUseCase.execute(email, oldPassword, newPassword, newPassword);
+
+        //THEN
+        verify(userRepositoryJpa, times(1)).findByEmail(any());
+        verify(userRepositoryJpa, times(1)).save(any());
+    }
+
+    private UserEntity buildUser(String password) {
+        UserEntity user = new UserEntity();
+
+        user.setId((long)1);
+        user.setName("Maria");
+        user.setSurname("Duque");
+        user.setNickname("mdr10");
+        user.setEmail("i92durom@uco.es");
+        user.setPassword(password);
+        user.setConfirmPassword(password);
+
+        return user;
+    }
+}
